417bb3c950d7d0241e8549e21c571e9e24d20e10
FlyingFish800 <58124651+FlyingFish800@users.noreply.github.com>
2021-09-22 21:47:09 -0700

    Sept 22
    Working on making Pong run, got to main loop

---
+++ Chip8Display.java
@@ -0,0 +1,117 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import javax.swing.JFrame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Chip8Display implements KeyListener{
+
+    final int WIDTH = 64;
+    final int HEIGHT = 32;
+    final int PANEL_WIDTH = 130;
+	final int MAX_KEYS = 111; // Up to numpad /
+
+    JFrame jf;
+    Canvas canv;
+    Graphics g;
+	private BufferStrategy bs;
+    int scale;
+	boolean keys[];
+	boolean pauseClock;
+
+    public Chip8Display(int scale) {
+        this.scale = scale; 
+		keys = new boolean[MAX_KEYS];
+		pauseClock = false;
+        jf = new JFrame("Chip 8 interpreter"); 
+		canv = new Canvas();
+		Dimension dim = new Dimension(WIDTH*scale+PANEL_WIDTH,HEIGHT*scale);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setResizable(true);
+		jf.setLocationRelativeTo(null);
+		jf.setVisible(true);
+		canv.setPreferredSize(dim);
+		canv.setMaximumSize(dim);
+		canv.setMinimumSize(dim);
+		canv.setSize(dim);
+		canv.setFocusable(false);
+		canv.addKeyListener(this);
+		jf.addKeyListener(this);
+		jf.add(canv);
+		jf.pack();
+    }
+
+    public void Refresh(boolean[][] screenData, Chip8CPU cpu){
+        bs = canv.getBufferStrategy();
+		if(bs==null) {
+			canv.createBufferStrategy(3);
+			return;
+		}
+		g = bs.getDrawGraphics();
+
+		//draw
+		g.clearRect(0, 0, WIDTH*scale+PANEL_WIDTH, HEIGHT*scale);
+        g.setColor(Color.BLACK);
+		
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if(screenData[x][y]) g.fillRect(x*scale, y*scale, scale, scale);
+            }
+        }
+		g.setColor(Color.RED);
+		g.fillRect(WIDTH*scale, 0, PANEL_WIDTH, HEIGHT*scale);
+		g.setColor(Color.WHITE);
+		int currentYpos = 16;
+		g.drawString("Paused: " + pauseClock, WIDTH*scale+4, currentYpos);
+		currentYpos += 16;
+		g.drawString("Instruction: 0x" + cpu.getIR(), WIDTH*scale+4, currentYpos);
+		int[] registers = cpu.getRegisters();
+		for (int i = 0; i < registers.length; i++) {
+			currentYpos += 16;
+			g.drawString("Register["+i+"]: " + registers[i], WIDTH*scale+4, currentYpos);
+		}
+		currentYpos += 16;
+		g.drawString("STACK", WIDTH*scale+4, currentYpos);
+		int[] stack = cpu.getStack();
+		for (int i = 0; i < stack.length; i++) {
+			currentYpos += 16;
+			g.drawString("["+i+"]: " + stack[i], WIDTH*scale+4, currentYpos);
+		}
+		//end draw
+		bs.show();
+		g.dispose();
+    }
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+		if(e.getKeyCode() == 27){
+			pauseClock = !pauseClock;
+		} else if(e.getKeyCode() == 16){
+			Chip8Main.pulse();
+		}else if(e.getKeyCode() == 8){
+			Chip8Main.reset();
+		}
+		
+		//System.out.println(e.getKeyCode());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean[] getKeys(){
+		return keys;
+	}
+}
---
+++ Chip8Main.java
@@ -0,0 +1,67 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Chip8Main extends Thread{
+
+    static final long SLEEP_TIME = 1000/60;
+
+    static Chip8CPU cpu;
+    static Chip8Display display;
+
+    @Override
+    public void run() {
+        while (true) {
+            //System.out.println(SLEEP_TIME);
+            if(!display.pauseClock) cpu.tick(display.getKeys());
+            display.Refresh(cpu.getScreenData(), cpu);
+            //cpu.printKeys(display.getKeys());
+            try {
+                Thread.sleep(SLEEP_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static byte[] getProgram(String file) throws IOException {
+        FileInputStream fileStream = new FileInputStream(file);
+        DataInputStream dataStream = new DataInputStream(fileStream);
+        byte[] data = new byte[dataStream.available()];
+        short index = 0;
+
+        while (dataStream.available() > 0){
+            data[index] = dataStream.readByte();
+            index++;
+        } // end while
+        dataStream.close();
+        fileStream.close();
+        return data;
+    }
+
+    public static void main(String[] args) {
+        Chip8Main chipInterpreter = new Chip8Main();
+        cpu = new Chip8CPU();
+        display = new Chip8Display(12);
+        try {
+            cpu.loadProg(getProgram("pong2.c8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        chipInterpreter.start();
+    } // end main
+
+    public static void pulse() {
+        cpu.tick(display.getKeys());
+    }
+
+    public static void reset() {
+        cpu.reset();
+        try {
+            cpu.loadProg(getProgram("pong2.c8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+} //end class
